9bb5a13ca027ab2afe264e0f64111df6530d4cf7
AumOzaa <programminglaptop1@gmail.com>
2026-04-05 11:43:11 +0530

    Writing files

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
 )
 
 func main() {
+	// Listing files
+	fmt.Println("--------- Exisiting Files ---------")
+	listFiles()
+	fmt.Println("-------------------------------")
+
+	fmt.Println("--------- Writing the file ---------")
+	writingAFile()
+	fmt.Println("-------------------------------")
+}
+
+func listFiles() {
 	files, err := os.ReadDir(".")
 	if err != nil {
 		log.Fatal(err)
@@ -15,4 +27,28 @@ func main() {
 	for _, file := range files {
 		fmt.Println(file.Name())
 	}
+}
+
+func MakeFIle() (*os.File, error) {
+	file, err := os.Create("./data/output1.txt")
+	if err != nil {
+		log.Fatal("Error creating the file")
+	}
+
+	// defer file.Close() #TODO: Do not close now will close when it's work is over in some other function
+
+	return file, err
+}
+
+func writingAFile() {
+	file, _ := MakeFIle()
+
+	defer file.Close()
+
+	w := bufio.NewWriter(file)
+	fmt.Fprint(w, "Hello, ")
+	fmt.Fprint(w, "world! ")
+	w.Flush()
+
+	fmt.Println()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/AumOzaa/fileProcessor
 
 go 1.26.1
+
+require (
+	github.com/sirupsen/logrus v1.9.4
+	golang.org/x/tools v0.43.0
+)
+
+require golang.org/x/sys v0.42.0 // indirect
