651818cad066de2f5dd69d1b13cd2aa08bf18b42
AumOzaa <programminglaptop1@gmail.com>
2026-04-05 14:41:07 +0530

    Reading lines

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"go/scanner"
 	"log"
 	"os"
 )
@@ -30,7 +31,7 @@ func listFiles() {
 }
 
 func MakeFIle() (*os.File, error) {
-	file, err := os.Create("./data/output1.txt")
+	file, err := os.Create("./data/output1.txt") // Assuming the directory already exists
 	if err != nil {
 		log.Fatal("Error creating the file")
 	}
@@ -43,7 +44,7 @@ func MakeFIle() (*os.File, error) {
 func writingAFile() {
 	file, _ := MakeFIle()
 
-	defer file.Close()
+	defer file.Close() // This'd close the file after the wiritingAfile func is completed
 
 	w := bufio.NewWriter(file)
 	fmt.Fprint(w, "Hello, ")
@@ -51,4 +52,23 @@ func writingAFile() {
 	w.Flush()
 
 	fmt.Println()
+}
+
+func countingLines(filePath string) (int, error) {
+	file, err := os.Open(filePath)
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	lineCount := 0
+
+	for scanner.Scan() {
+		lineCount++
+	}
+
+	if err := scanner.Err(); err != nil {
+		return 0, err
+	}
+
+	return lineCount, err
 }
